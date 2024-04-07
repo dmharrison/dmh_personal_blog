a66dd42e3e6f516b69ff2387c1608f0d7e5b6e3d
dmharrison <dmharrison2014@gmail.com>
2024-04-07 00:06:14 -0400

    updated blog html and css

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,21 +1,26 @@
-// recall the local storage inforamtion and append it on the page
+// need to make a button to go back to previous page
+document.getElementById("backbutton").addEventListener('click', function(){
+    window.history.back()
+})
 
+
+// recall the local storage inforamtion and append it on the page
 
 const displayBlog = function() {
     let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
     let blog = document.querySelector('#blog');
     
-
-    blogPosts.forEach(blogPost => {
+// iterate over each blog post and create elements for the username, title and story
+    blogPosts.forEach(function(blogPost) {
         let usernameElement = document.createElement('h2');
-        usernameElement.textContent = `Author: ${blogPost.username}`;
+        usernameElement.textContent = `${blogPost.username}`;
 
         let titleElement = document.createElement('h3');
         titleElement.textContent = blogPost.title;
 
         let storyElement = document.createElement('p');
         storyElement.textContent = blogPost.story;
-
+// append them to the blog element
         blog.appendChild(usernameElement);
         blog.appendChild(titleElement);
         blog.appendChild(storyElement);
@@ -23,3 +28,4 @@ const displayBlog = function() {
 }
 
 displayBlog();
+
